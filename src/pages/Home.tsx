6228a33e394d6c57571c4f32cd5e1f1310469c98
DTSIRIN <boat_num@hotmail.com>
2020-12-06 14:58:57 +0700

    -educate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton,IonInput } from '@ionic/react';
+import React from  'react';
+
 import ExploreContainer from '../components/ExploreContainer';
+import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 import './Home.css';
+
 
 const Home: React.FC = () => {
   return (
@@ -13,12 +16,42 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            <IonCardTitle>PEWPEW</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            Keep close to Nature's heart... and break clear away, once in awhile,
+            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+      </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem>
+            <IonIcon icon={pin} slot="start" />
+            <IonLabel>ion-item in a card, icon left, button right</IonLabel>
+            <IonButton fill="outline" slot="end">View</IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            This is content, without any paragraph or header tags,
+            within an ion-cardContent element.
+           </IonCardContent>
+
+          <IonItem>
+            <IonLabel position="stacked">Email</IonLabel>
+            <IonInput placeholder="Email"> </IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Password</IonLabel>
+            <IonInput placeholder="Password" type="password"> </IonInput>
+          </IonItem>
+        </IonCard>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+       
+      </IonContent>z
     </IonPage>
   );
 };
